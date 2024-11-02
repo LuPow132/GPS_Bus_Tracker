52134ba2fb8df3a4cbae98d48f6ea42733e1462f
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-11-02 15:29:11 +0700

    Update GPS_ESP32.ino

--- Example/GPS_ESP32.ino
+++ Example/GPS_ESP32.ino
@@ -17,8 +17,6 @@ void setup()
 Serial.begin(115200);
 ss.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin, false);
 Serial.println(TinyGPSPlus::libraryVersion());
-lcd.setCursor(0, 0);
-lcd.print("No GPS Signal...");
 }
 
 void loop()
@@ -37,15 +35,17 @@ while(true);
 
 void displayInfo()
 {
-if(gps.location.isUpdated()){
-    lcd.clear(); 
-    lcd.setCursor(0, 0);
-    lcd.print(gps.location.lat(), 6);
-    lcd.setCursor(0, 1);
-    lcd.print(gps.location.lng(), 6);
-  }
+Serial.print(F("Location: "));
+if (gps.location.isUpdated())
+{
+Serial.print(gps.location.lat(), 6);
+Serial.print(F(","));
+Serial.print(gps.location.lng(), 6);
+}
+else
+{
+Serial.print(F("INVALID"));
+}
 
 Serial.println();
 }
-
-
